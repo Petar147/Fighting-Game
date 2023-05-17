4ddfd64f08cac3dc0511b2a83a7b9c4531027511
petarnikolic <petar147nikolic@gmail.com>
2023-05-17 16:47:01 +0200

    health bar logic

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ class Sprite {
     };
     this.color = color;
     this.isAttacking = false;
+    this.health = 100;
   }
 
   draw() {
@@ -163,6 +164,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false;
+    enemy.health -= 20;
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
 
   if (
@@ -173,6 +176,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false;
+    player.health -= 20;
+    document.querySelector("#playerHealth").style.width = player.health + "%";
   }
 }
 animate();
