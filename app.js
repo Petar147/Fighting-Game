949f08965e2657439d058043edd4dfc768275e6b
petarnikolic <petar147nikolic@gmail.com>
2023-05-18 15:03:04 +0200

    player sprite idle animation

--- app.js
+++ app.js
@@ -42,6 +42,13 @@ const player = new Fighter({
     x: 0,
     y: 0,
   },
+  imageSrc: "./img/player/Idle.png",
+  framesMax: 8,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 157,
+  },
 });
 
 const enemy = new Fighter({
@@ -87,7 +94,7 @@ function animate() {
   background.update();
   shop.update();
   player.update();
-  enemy.update();
+  // enemy.update();
 
   player.velocity.x = 0;
   enemy.velocity.x = 0;
--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,11 @@
 class Sprite {
-  constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
+  constructor({
+    position,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+  }) {
     this.position = position;
     this.width = 50;
     this.height = 150;
@@ -10,6 +16,7 @@ class Sprite {
     this.frameCurrent = 0;
     this.framesElapsed = 0;
     this.framesHold = 8;
+    this.offset = offset;
   }
 
   draw() {
@@ -20,16 +27,16 @@ class Sprite {
       this.image.width / this.framesMax,
       this.image.height,
 
-      this.position.x,
-      this.position.y,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
       (this.image.width / this.framesMax) * this.scale,
       this.image.height * this.scale
     );
   }
 
-  update() {
-    this.draw();
+  animateFrames() {
     this.framesElapsed++;
+
     if (this.framesElapsed % this.framesHold === 0)
       if (this.frameCurrent < this.framesMax - 1) {
         this.frameCurrent++;
@@ -37,11 +44,31 @@ class Sprite {
         this.frameCurrent = 0;
       }
   }
+
+  update() {
+    this.draw();
+    this.animateFrames();
+  }
 }
 
-class Fighter {
-  constructor({ position, velocity, color, offset }) {
-    this.position = position;
+class Fighter extends Sprite {
+  constructor({
+    position,
+    velocity,
+    color,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+  }) {
+    super({
+      position,
+      imageSrc,
+      scale,
+      framesMax,
+      offset,
+    });
+
     this.velocity = velocity;
     this.width = 50;
     this.height = 150;
@@ -58,27 +85,17 @@ class Fighter {
     this.color = color;
     this.isAttacking = false;
     this.health = 100;
+    this.frameCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 8;
   }
 
-  draw() {
-    c.fillStyle = this.color;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-    // attack box
-    if (this.isAttacking) {
-      c.fillStyle = "green";
-      c.fillRect(
-        this.attackBox.position.x,
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height
-      );
-    }
-  }
   //update metoda za pomeranje objekata okolo
 
   update() {
     this.draw();
+    this.animateFrames();
+
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
     this.attackBox.position.y = this.position.y;
 
